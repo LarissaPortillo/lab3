da990a31e8f50dcbccd7f45ffc5d8788d60287d7
Glitch (hello-webpage) <none>
2020-09-29 21:43:28 +0000

    🐒🏡 Checkpoint ./script.js:21940894/398

--- script.js
+++ script.js
@@ -5,9 +5,10 @@
 console.log("hi");
 
 d3.csv('https://cdn.glitch.com/13954448-9de1-4018-9e2a-9b65a98670e1%2Fcities.csv?v=1601412428740', d3.autoType).then(data=>{
-	console.log('cities', data);
+	console.log('cities', filterEU(data));
 });
 
-function filterData(country){
-  
+
+function filterEU(d){
+  return d.filter(l=> l.eu == true)
 };
