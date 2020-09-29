a18f206ed39f0c595d2af940396c723ba8ed47a7
Glitch (hello-webpage) <none>
2020-09-29 22:06:29 +0000

    🦁🐠 Checkpoint ./script.js:21940894/68 ./index.html:21940894/223

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+
 
 d3.csv('https://cdn.glitch.com/13954448-9de1-4018-9e2a-9b65a98670e1%2Fcities.csv?v=1601412428740', d3.autoType).then(data=>{
 	console.log('cities', filterEU(data));
