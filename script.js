2d6b87fd2592168a21e2ad66e6f67933049b4497
Glitch (hello-webpage) <none>
2020-09-29 23:52:50 +0000

    🎆💆 Checkpoint ./script.js:21940894/529

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ d3.csv('https://cdn.glitch.com/13954448-9de1-4018-9e2a-9b65a98670e1%2Fcities.csv
   .then(data=>{
   data = data.filter(d=>d.eu == true);
   console.log('cities', data);
-  d3.select('.city-count').text(...);
+  d3.select('.city-count').text('27');
                               
-  svg.selectAll(...)
+  svg.selectAll('circle')
   .data(data)
   .enter()
-  .append(...)
-  .attr('cx', (d,i)=>(i+1)*30)
-  .attr()
+  .append('circle')
+  .attr('cx', function (d,i){
+    return i*100;
+  });
+  
 });
 
 
