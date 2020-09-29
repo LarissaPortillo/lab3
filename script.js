5fe8f946eb86838fbdcb460506265f0cb46f0d2f
Glitch (hello-webpage) <none>
2020-09-29 20:57:07 +0000

    🛣👨 Checkpoint ./.glitch-assets:21940894/352 ./script.js:21940894/879 ./index.html:21940894/597

--- script.js
+++ script.js
@@ -3,3 +3,8 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+d3.csv('https://cdn.glitch.com/13954448-9de1-4018-9e2a-9b65a98670e1%2Fcities.csv?v=1601412428740').then(data=>{
+	console.log('cities', data);
+})
+
