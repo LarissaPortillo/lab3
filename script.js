3468adf45efb03ea5d85c81685b1b2fbc5888a15
Glitch (hello-webpage) <none>
2020-09-30 00:28:31 +0000

    🌟📡 Checkpoint ./index.html:21940894/48 ./script.js:21940894/44

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ d3.csv('https://cdn.glitch.com/13954448-9de1-4018-9e2a-9b65a98670e1%2Fcities.csv
   
   d3.select('.city-count').text();
                               
-  svg.selectAll('.population-plot')
+  svg.selectAll('circle')
   .data(data)
   .enter()
-  .append('.population-plot')
+  .append('circle')
   .attr('cx', function (d,i){
     return i*100;
   });
